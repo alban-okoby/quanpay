d3714d7b8e860adeeed724ab9b38b7965afaa711
Alban Kedi <albanokoby225@gmail.com>
2025-03-29 12:52:52 +0000

    Creted inbloc dashboard page (without any sections)

---
+++ webapp/public/assets/js/config.js
@@ -0,0 +1,8 @@
+'use strict';
+var theme_contrast = 'false'; // [ false , true ]
+var caption_show = 'true'; // [ false , true ]
+var preset_theme = 'preset-1'; // [ preset-1 dash for quantech]
+var dark_layout = 'false'; // [ false , true , default ]
+var rtl_layout = 'false'; // [ false , true ]
+var box_container = 'false'; // [ false , true ]
+var version = '1.0';
